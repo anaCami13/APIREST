a05eaeddfbc9b09b3383d4f5770863d5072242ce
Cami <21240071@leon.tecnm.mx>
2024-10-16 14:27:38 -0600

    Api RES Version 1, Main

---
+++ api-res/prueba.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+# URL a la que envías la solicitud POST
+url = 'http://localhost:3000/newtask'
+
+# Datos que quieres enviar
+data = {
+    'Name': "Task Tree",
+    'Content': 'Content Tree'
+}
+
+# Realizar la solicitud POST con el formato JSON
+response = requests.post(url, headers={'Content-Type': 'application/json'}, data=json.dumps(data))
+
+# Verificar el código de estado y el contenido de la respuesta
+if response.status_code == 200:
+    print('Datos enviados exitosamente:', response.json())
+else:
+    print(f'Error {response.status_code}: {response.text}')
